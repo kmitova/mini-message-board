b8fef41bf041f433ba1cc4e0fcd2d54d000b0c1c
kmitova <96522051+kmitova@users.noreply.github.com>
2023-03-12 09:12:38 +0200

    routing, views, form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Jim",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Michael",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res) {
+  res.render("form", { title: "Mini Message Board" });
+});
+
+router.post("/new", function (req, res) {
+  let data = req.body;
+  let messageText = data.content;
+  let messageUser = data.author;
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+
+  res.redirect("/");
 });
 
 module.exports = router;
